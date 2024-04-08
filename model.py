ee49b1b58d4e7908aa0f0e7030330347d5bf15d0
zeonfaiho <zeonfaiho@gmail.com>
2024-04-08 10:46:15 +0800

    update

--- model.py
+++ model.py
@@ -135,8 +135,8 @@ class Block(nn.Module):
         self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
         self.attn = CausalSelfAttention(config)
         self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
-        self.mlp = MLP(config)
-        # self.mlp = LoReGLU(config)
+        # self.mlp = MLP(config)
+        self.mlp = LoReGLU(config)
         # self.mlp = ReGLU(config)
 
     def forward(self, x):
